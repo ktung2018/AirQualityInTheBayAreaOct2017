a899b138ab73b6cf80ceed646454bc58b325723a
GAMES2018 <35645038+ktung2018@users.noreply.github.com>
2018-12-03 08:14:26 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+api_key = "REMOVED"
+=======
+api_key = "Your Key Here"
+>>>>>>> hadrien-new-feat
+# Source: https://docs.airnowapi.org/HistoricalObservationsByZip/docs
---
+++ config3.py
@@ -0,0 +1,2 @@
+api_key = "INSERT YOUR KEY HERE"
+# Source: https://docs.airnowapi.org/HistoricalObservationsByZip/docs
